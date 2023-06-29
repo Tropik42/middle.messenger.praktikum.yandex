f73ef8060a2d3f20f412b5f4861a3b2af1c785d2
Tropik42 <aviavova94@gmail.com>
2023-06-30 00:31:17 +0300

    test rollup

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,15 @@ import handlebars from './vite-plugin-handlebars=precompile';
 export default defineConfig({
     root: resolve(__dirname, 'src'),
     build: {
+        rollupOptions: {
+            input: {
+                register: resolve(
+                    __dirname,
+                    'src/pages/register/register.html'
+                ),
+                login: resolve(__dirname, 'src/pages/login/login.html'),
+            },
+        },
         outDir: resolve(__dirname, 'dist'),
     },
     plugins: [handlebars()],
