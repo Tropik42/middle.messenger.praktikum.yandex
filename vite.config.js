c725030a1350351f81f04a685afc452b8272b885
Tropik42 <aviavova94@gmail.com>
2023-06-30 01:37:46 +0300

    test rollup 9

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,31 @@
-import {resolve} from 'path';
 import {defineConfig} from 'vite';
-import handlebars from './vite-plugin-handlebars=precompile';
+import {ViteAliases} from 'vite-aliases';
+// import checker from 'vite-plugin-checker';
+import legacy from '@vitejs/plugin-legacy';
+import stylelint from 'vite-plugin-stylelint';
+import handlebarsPrecompile from './src/utils/vite-plugin-handlebars-precompile';
 
 export default defineConfig({
-    root: resolve(__dirname, 'src'),
     build: {
-        outDir: resolve(__dirname, 'dist'),
+        target: 'es2017',
+        outDir: 'dist',
     },
-    plugins: [handlebars()],
+    server: {
+        port: 3000,
+        host: '0.0.0.0',
+        hmr: true,
+    },
+    plugins: [
+        ViteAliases(),
+        // stylelint({fix: true}),
+        handlebarsPrecompile(),
+        // checker({
+        //     eslint: {
+        //       lintCommand: 'eslint "./src/**/*.{ts,tsx}"'
+        //     },
+        //   }),
+        legacy({
+            targets: ['defaults', 'not IE 11'],
+        }),
+    ],
 });
