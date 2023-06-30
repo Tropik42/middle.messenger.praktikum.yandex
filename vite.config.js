2511aba96b5e5ea0cecc5d275e4698dabb558a1c
Tropik42 <aviavova94@gmail.com>
2023-06-30 23:25:47 +0300

    delete commented strings

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import {defineConfig} from 'vite';
 import {ViteAliases} from 'vite-aliases';
-// import checker from 'vite-plugin-checker';
 import legacy from '@vitejs/plugin-legacy';
-// import stylelint from 'vite-plugin-stylelint';
 import handlebarsPrecompile from './src/utils/vite-plugin-handlebars-precompile';
 
 export default defineConfig({
@@ -17,13 +15,7 @@ export default defineConfig({
     },
     plugins: [
         ViteAliases(),
-        // stylelint({fix: true}),
         handlebarsPrecompile(),
-        // checker({
-        //     eslint: {
-        //       lintCommand: 'eslint "./src/**/*.{ts,tsx}"'
-        //     },
-        //   }),
         legacy({
             targets: ['defaults', 'not IE 11'],
         }),
