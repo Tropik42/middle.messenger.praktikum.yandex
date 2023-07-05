e49157d6e66c8970fbdee3bce913a043f54ae86d
Tropik42 <aviavova94@gmail.com>
2023-07-06 01:15:41 +0300

    add router

---
+++ src/utils/router.ts
@@ -0,0 +1,48 @@
+class Router {
+    constructor(rootQuery) {
+        if (Router.__instance) {
+            return Router.__instance;
+        }
+
+        this.routes = [];
+        this.history = window.history;
+        this._currentRoute = null;
+        this._rootQuery = rootQuery;
+
+        Router.__instance = this;
+    }
+
+    use(pathname, block) {
+        const route = new Route(pathname, block, {rootQuery: this._rootQuery});
+        this.routes.push(route);
+    }
+
+    start() {
+    }
+
+    _onRoute(pathname) {
+        const route = this.getRoute(pathname);
+
+        if (this._currentRoute) {
+            this._currentRoute.leave();
+        }
+
+        this._currentRoute = route;
+        route.render(route, pathname);
+    }
+
+    go(pathname) {
+        this.history.pushState({}, '', pathname);
+        this._onRoute(pathname);
+    }
+
+    back() {
+    }
+
+    forward() {
+    }
+
+    getRoute(pathname) {
+        return this.routes.find(route => route.match(pathname));
+    }
+}
